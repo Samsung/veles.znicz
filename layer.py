34955dc905a013afa411f8b6d5bba866b63f4a61
Alexey Kazantsev <a.kazantsev@samsung.com>
2013-03-11 17:28:00 +0400

    Hello World

---
+++ layer.py
@@ -0,0 +1,16 @@
+"""
+Created on Mar 11, 2013
+
+One layer of the neural network
+
+@author: Kazantsev Alexey <a.kazantsev@samsung.com>
+"""
+class Layer(object):
+    """One Layer of the neural network
+    
+    One layer of the neural network
+    
+    Attributes:
+    """
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Created on Mar 11, 2013
+
+Entry point
+
+@author: Kazantsev Alexey <a.kazantsev@samsung.com>
+"""
+import layer
+
+
+def main():
+    l = layer.Layer()
+    print(l.__doc__)
+    print("End of job")
+
+
+if __name__ == '__main__':
+    main()
