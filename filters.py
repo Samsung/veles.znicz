32ad98e18fff08fcc6be7631a2d814302426f529
Alexey Kazantsev <a.kazantsev@samsung.com>
2013-03-12 12:35:41 +0400

    patched comments according to google style guide

--- filters.py
+++ filters.py
@@ -1,7 +1,7 @@
 """
 Created on Mar 12, 2013
 
-Filters in data stream neural network model
+Filters in data stream neural network model.
 
 @author: Kazantsev Alexey <a.kazantsev@samsung.com>
 """
@@ -9,38 +9,30 @@ from numpy import *
 
 
 class GeneralFilter(object):
-    """GeneralFilter
-    
-    General filter in data stream neural network model
-    
-    Attributes:
+    """General filter in data stream neural network model.
     """
     def __init__(self):
         super().__init__()
 
 
 class PrimitiveFilter(GeneralFilter):
-    """PrimitiveFilter
-    
-    Filter that cannot contain other filters
+    """Filter that cannot contain other filters.
     """
     def __init__(self):
         super().__init__()
 
 
 class FilterExists(Exception):
-    """Exception, raised when filter exists in the container
+    """Exception, raised when filter exists in the container.
     """
     pass
 
 
 class ContainerFilter(GeneralFilter):
-    """ContainerFilter
-    
-    Filter that contains other filters
+    """Filter that contains other filters.
     
     Attributes:
-        filters: set of filters in the container
+        filters: set of filters in the container.
     """
     def __init__(self):
         super().__init__()
@@ -51,18 +43,16 @@ class ContainerFilter(GeneralFilter):
             raise FilterExists
     
     def add(self, f):
-        """add
-        
-        Adds filter to the container
+        """Adds filter to the container.
         
         Args:
-            f: filter to add
+            f: filter to add.
         
         Returns:
-            f
+            f.
         
         Raises:
-            FilterExists
+            FilterExists.
         """
         self.check_exists(f)
         self.filters.add(f)
@@ -70,12 +60,10 @@ class ContainerFilter(GeneralFilter):
 
 
 class All2AllFilter(PrimitiveFilter):
-    """All2AllFilter
-    
-    Classical MLP layer to layer connection
+    """Classical MLP layer to layer connection.
     
     Attributes:
-        weights: matrix of weights
+        weights: matrix of weights.
     """
     def __init__(self):
         super().__init__()
