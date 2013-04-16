cc9d2e5a69f414769b6443f7b57b3e355a09b119
Denis Seresov <d.seresov@samsung.com>
2013-04-16 19:32:34 +0400

    more on text

--- text.py
+++ text.py
@@ -23,21 +23,82 @@ class TXTLoader(filters.Filter):
             return
         self.output = formats.Batch()
         self.labels = formats.Labels()
+        self.TrainIndex = formats.Labels()
+        self.ValidIndex = formats.Labels()
+        self.TestIndex  = formats.Labels()
+        
 
     def load_original(self):
         """ Loads data from original Wine dataset.
-        """
+            we will read config file with parameters dataset(width,height),
+             size(or %) train set,size(or %) validation set,size(or %) test set,
+             random or on series or random with seed...
+             type normalization dataset(range)
+             
+             
+        """ 
         print("One time relatively slow load from original Wine Dataset...")
+        
 
         self.labels.batch=filters.aligned_zeros([178])
         self.labels.n_classes=3
         self.output.batch=filters.realign(numpy.loadtxt("wine/wine.csv", numpy.float32).reshape([178,13]))
         self.labels.batch=filters.realign(numpy.loadtxt("wine/wine_y_labels.csv", numpy.float32).reshape([178,1]))
-        out =filters.aligned_zeros([178])
-        for i=1 in 10
-            print(i)
-        
+        self.labels.batch=filters.aligned_zeros([178])
         print("Done")
+
+    def normalize_use_range_train (self,Range):
+        """
+        """
+        self.normalize_x_use_range_train(self,Range)
+        self.normalize_y_use_range_train(self,Range)
+        
+    def normalize_use_all_dataset (self):
+        """
+        """        
+        self.normalize_x_use_all_dataset(self)
+        self.normalize_y_use_all_dataset(self)
+        
+    def normalize_x_use_all_dataset(self):
+        """ normalization  input data.
+        """    
+        self.outmean =filters.aligned_zeros([13])
+        
+        self.outmean= numpy.mean(self.output.batch, axis=0)
+        
+        self.outstd =filters.aligned_zeros([13])
+        self.outstd= numpy.std(self.output.batch, axis=0)
+        
+        self.output2 =filters.realign(filters.aligned_zeros([178*13]).reshape([178,13]))
+        self.output2[:]=self.output.batch[:]
+        
+        for i in range(0,13):
+            self.output2[:,i]=((self.output.batch[:,i]-self.outmean[i]))/self.outstd[i]
+        
+        self.outmin =filters.aligned_zeros([13])
+        self.outmin= numpy.min(self.output2, axis=0)
+        
+        self.outmax =filters.aligned_zeros([13])
+        self.outmax= numpy.max(self.output2, axis=0)
+        
+        for i in range(0,13):
+            self.output2[:,i]=((((self.output2[:,i]-self.outmin[i]))/(self.outmax[i]-self.outmin[i]))-0.5)*2
+        
+    def normalize_x_use_range_train (self,Range):
+        """
+        """    
+    def denormalize_x(self):
+        """
+        """
+    def normalize_y(self):
+        """
+        """
+    def denormalize_y(self):
+        """
+        """
+    def normalize_y_use_range_train (self,Range):
+        """
+        """            
     def initialize(self):
         """Here we will load Wine data.
         """
