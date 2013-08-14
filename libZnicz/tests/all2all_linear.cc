3ba87e9871449b6120db68bd04799372d0d240d0
Ernesto Sanches <ernestosanches@gmail.com>
2013-08-14 19:03:50 +0400

    Added All2All Linear unit tests
    Change-Id: I972b35e7ef7335dac5d6863e75ead90c3d991cc9

---
+++ libZnicz/tests/all2all_linear.cc
@@ -0,0 +1,66 @@
+/*! @file all2all_linear.cc
+ *  @brief "All to all" unit with linear activation function test
+ *  @author Ernesto Sanches <ernestosanches@gmail.com>
+ *  @version 1.0
+ *
+ *  @section Notes
+ *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
+ *
+ *  @section Copyright
+ *  Copyright 2013 Samsung R&D Institute Russia
+ */
+
+
+#include <veles/unit_registry.h>
+#include "src/all2all_linear.h"
+#include "tests/all2all_linear.h"
+
+const size_t All2AllLinearSquare::kCount = 5;
+
+TEST_F(All2AllLinear, Execution) {
+  float weights_f[kInputs * kOutputs] = {1, 2.1, 33,
+                                         4, 55, 6 };
+  float bias_f[kOutputs] = {433, 42.9};
+  float expected[kOutputs] = {1964.362, 2800.2};
+  auto weights = CreateFloatArray(kInputs * kOutputs);
+  auto bias = CreateFloatArray(kOutputs);
+  memcpy(weights.get(), weights_f, sizeof(weights_f));
+  memcpy(bias.get(), bias_f, sizeof(bias_f));
+  unit_->SetParameter("weights", weights);
+  unit_->SetParameter("bias", bias);
+  unit_->Execute(input_.get(), output_.get());
+  for(size_t i = 0; i < kOutputs; ++i) {
+    EXPECT_FLOAT_EQ(expected[i], output_.get()[i]);
+  }
+}
+
+TEST_F(All2AllLinearSquare, ExecutionIdentity) {
+  auto weights = CreateFloatArray(kCount * kCount);
+  for(size_t i = 0; i < kCount; ++i) {
+    weights.get()[(kCount + 1) * i] = kValueOne;
+  }
+  unit_->SetParameter("weights", weights);
+  unit_->Execute(input_.get(), output_.get());
+  for(size_t i = 0; i < kCount; ++i) {
+    EXPECT_EQ(kValueInputInit, output_.get()[i]);
+  }
+}
+
+TEST_F(All2AllLinearSquare, ExecutionBias) {
+  auto bias = CreateFloatArray(kCount * kCount, kValueOther);
+  unit_->SetParameter("bias", bias);
+  unit_->Execute(input_.get(), output_.get());
+  for(size_t i = 0; i < kCount; ++i) {
+    EXPECT_EQ(kValueOther, output_.get()[i]);
+  }
+}
+
+namespace Veles {
+namespace Znicz {
+
+REGISTER_UNIT(All2AllLinear);
+
+}
+}
+
+#include "tests/google/src/gtest_main.cc"
---
+++ libZnicz/tests/all2all_linear.h
@@ -0,0 +1,37 @@
+/*! @file all2all_linear.h
+ *  @brief "All to all" unit with linear activation function test
+ *  @author Ernesto Sanches <ernestosanches@gmail.com>
+ *  @version 1.0
+ *
+ *  @section Notes
+ *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
+ *
+ *  @section Copyright
+ *  Copyright 2013 Samsung R&D Institute Russia
+ */
+
+#ifndef TESTS_ALL2ALL_LINEAR_H_
+#define TESTS_ALL2ALL_LINEAR_H_
+
+#include "tests/all2all.h"
+
+class All2AllLinear : public All2AllTest {
+ protected:
+  All2AllLinear() : All2AllTest("All2All") {
+  }
+
+  virtual void SetUp() override {
+    Initialize(kInputs, kOutputs);
+  }
+};
+
+class All2AllLinearSquare : public All2AllLinear {
+ protected:
+  static const size_t kCount;
+
+  virtual void SetUp() override {
+    Initialize(kCount, kCount);
+  }
+};
+
+#endif  // TESTS_ALL2ALL_H_
