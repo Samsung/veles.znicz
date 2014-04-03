d8e4d6da1ed09f023d950fba0c159feb0835553f
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-04-03 17:38:12 +0400

    Added new functional test for wine workflow
    Change-Id: I881270e64c93634632616929add0d62580bc5886

---
+++ tests/functional/__init__.py
@@ -0,0 +1,6 @@
+"""
+Created on April 3, 2014
+
+@author: Podoynitsina Lyubov <lyubov.p@samsung.com>
+"""
+
---
+++ tests/functional/test_wine.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3.3 -O
+"""
+Created on November 18, 2013
+
+@author: Lyubov Podoynitsina <lyubov.p@samsung.com>
+"""
+
+
+import logging
+import numpy
+import os
+import unittest
+
+from veles.config import root
+import veles.launcher as launcher
+import veles.opencl as opencl
+import veles.rnd as rnd
+import veles.znicz.samples.wine as wine
+
+
+class TestWorkflow(unittest.TestCase):
+    def setUp(self):
+        root.common.unit_test = True
+        root.common.plotters_disabled = True
+        self.device = opencl.Device()
+
+    def tearDown(self):
+        del self.device
+
+    def test_worflow(self):
+        logging.info("Will test loader, decision, evaluator units")
+        rnd.default.seed(numpy.fromfile("%s/veles/znicz/samples/seed" %
+                                        (root.common.veles_dir),
+                                        dtype=numpy.int32, count=1024))
+        root.common.update = {"plotters_disabled": True}
+
+        root.update = {"decision": {"fail_iterations": 200,
+                                    "snapshot_prefix": "wine"},
+                       "global_alpha": 0.75,
+                       "global_lambda": 0.0,
+                       "layers":  [8, 3],
+                       "loader": {"minibatch_maxsize": 10},
+                       "path_for_load_data":
+                       os.path.join(root.common.veles_dir,
+                                    "veles/znicz/samples/wine/wine.data")}
+        l = launcher.Launcher()
+        device = None if l.is_master else opencl.Device()
+        w = wine.Workflow(l, layers=[8, 3], device=device)
+        w.initialize()
+        l.run()
+        epoch = w.decision.epoch_number[0]
+        self.assertEqual(epoch, 11)
+        logging.info("All Ok")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
