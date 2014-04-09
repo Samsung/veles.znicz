2a7b84acacbc045b10b25634972cd0de3a9a30ad
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-04-09 19:41:27 +0400

    Created accumulator unit for calculate output of forward
    Change-Id: I87bbf1a8cd75b594c3cfe4cf8e22ddcc62fa2f7e

---
+++ accumulator.py
@@ -0,0 +1,55 @@
+"""
+Created on April 9, 2014
+
+Accumulator units.
+
+@author: Lyubov Podoynitsina <lyubov.p@samsung.com>
+"""
+
+
+import numpy
+
+from veles.config import root, get_config
+import veles.formats as formats
+from veles.mutable import Bool
+import veles.units as units
+
+
+class RangeAccumulator(units.Unit):
+    """Range accumulator.
+    """
+    def __init__(self, workflow, **kwargs):
+        super(RangeAccumulator, self).__init__(workflow)
+        bars = kwargs.get("bars", get_config(root.n_bars, 30))
+        kwargs["bars"] = bars
+        self.input = None
+        self.output = formats.Vector()
+        self.reset_flag = Bool(True)
+        self.n_bars = [0]
+        self.bars = bars
+        self.max = get_config(root.accumulator.max, 1.7159)
+        self.min = get_config(root.accumulator.min, -1.7159)
+
+    def initialize(self):
+        super(RangeAccumulator, self).initialize()
+        self.output.v = numpy.zeros([self.bars + 2], dtype=numpy.int64)
+
+    def run(self):
+        d = self.max - self.min
+        if not d:
+            return
+        self.output.map_write()
+        self.input.map_read()
+        d = (self.bars - 1) / d
+        if self.reset_flag:
+            self.output.v[:] = 0
+        self.n_bars[0] = self.bars + 2
+        for y in self.input.v.ravel():
+            if y < self.min:
+                self.output[0] += 1
+                continue
+            if y <= self.max and y > self.min:
+                i = int(numpy.floor((y - self.min) * d))
+                self.output[i] += 1
+                continue
+            self.output[self.bars + 1] += 1
