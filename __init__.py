0a6c72304ecd5a9dbacf5af1cd364ae15e8fa687
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-04-14 14:02:32 +0400

    Added metadata to __init__
    Change-Id: I72bb9bac69e7f00744106198c5304c2944672f94

--- __init__.py
+++ __init__.py
@@ -5,7 +5,15 @@ Created on May 21, 2013
 """
 
 
+from email.utils import parsedate_tz, mktime_tz
 import os
 from veles.config import root
 
 root.common.ocl_dirs.append(os.path.join(os.path.dirname(__file__), "ocl"))
+
+
+__git__ = "$Commit$"
+__date__ = mktime_tz(parsedate_tz("$Date$"))
+__version__ = "0.2.0"
+__license__ = "Samsung Proprietary License"
+__copyright__ = "© 2013 Samsung Electronics Co., Ltd."
