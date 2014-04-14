282ded330318a6a646e2aa80c742e599427bb432
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-04-14 18:07:18 +0400

    Added fill_minibatch(), labels, refactored force in Imagenet loader
    Change-Id: Ib6d095e8057f5a2d0b92b52f0d939c1a38d6e9f4

--- samples/imagenet/loader.py
+++ samples/imagenet/loader.py
@@ -5,6 +5,7 @@ Created on Apr 10, 2014
 """
 
 
+from concurrent.futures import ThreadPoolExecutor
 import cv2
 import jpeg4py
 import json
@@ -17,6 +18,7 @@ import xmltodict
 
 import veles.config as config
 import veles.opencl_types as opencl_types
+from veles.external.prettytable import PrettyTable
 import veles.znicz.loader as loader
 
 
@@ -66,10 +68,15 @@ class Loader(loader.Loader):
         self._sobel_kernel_size = kwargs.get(
             "sobel_kernel_size",
             config.get(config.root.imagenet.sobel_ksize) or 5)
+        self._force_reinit = kwargs.get(
+            "force_reinit",
+            config.get(config.root.imagenet.force_reinit) or False)
 
     def init_unpickled(self):
         super(Loader, self).init_unpickled()
         self._db_ = leveldb.LevelDB(self._dbpath)
+        self._executor_ = ThreadPoolExecutor(
+            config.get(config.root.imagenet.thread_pool_size) or 4)
 
     @property
     def images_path(self):
@@ -90,18 +97,32 @@ class Loader(loader.Loader):
     def load_data(self):
         self._init_files()
         self._init_metadata()
+        self._init_labels()
         self._fill_class_samples()
 
     def create_minibatches(self):
         count = self.minibatch_maxsize
-        minibatch_shape = [count, 3] + list(self._data_shape)
+        minibatch_shape = [count] + list(self._data_shape) + \
+            [3 + (1 if self._include_derivative else 0)]
         self.minibatch_data << numpy.zeros(shape=minibatch_shape,
                                            dtype=self._dtype)
         self.minibatch_labels << numpy.zeros(count, dtype=numpy.int32)
         self.minibatch_indexes << numpy.zeros(count, dtype=numpy.int32)
 
     def fill_minibatch(self):
-        pass
+        images = self._executor_.map(
+            lambda i: (i, self._get_sample(self.shuffled_indexes[i])),
+            range(self.minibatch_size))
+        for i, data in images:
+            self.minibatch_data[i] = data
+        for i in range(self.minibatch_size):
+            try:
+                meta = self._get_meta(self.shuffled_indexes[i])
+                name = meta["object"]["name"]
+            except KeyError:
+                fn = self._get_file_name(self.shuffled_indexes[i])
+                name = os.path.basename(os.path.dirname(fn))
+            self.minibatch_labels[i] = self._label_map[name]
 
     def _get_file_name(self, index):
         for i in range(len(self._files_locator) - 1):
@@ -236,16 +257,17 @@ class Loader(loader.Loader):
             return res
         return path
 
-    def _init_files(self, force=False):
+    def _init_files(self):
         self.debug("Initializing files table...")
         files_key = ("files_%s_%s" % (self.year, self.series)).encode()
-        if not force:
+        if not self._force_reinit:
             try:
                 files = self._db_.Get(files_key)
                 self.info("Loaded files table from DB")
                 self._files = json.loads(files.decode())
                 do_init = False
             except KeyError:
+                self.info("Initializing files table from scratch...")
                 do_init = True
         else:
             do_init = True
@@ -257,7 +279,7 @@ class Loader(loader.Loader):
                 imgs = []
                 subdir = years[self.year][self.series][0]
                 path = os.path.join(self._ipath, subdir)
-                self.debug("Scanning %s...", path)
+                self.info("Scanning %s...", path)
                 for root, _, files in os.walk(path, followlinks=True):
                     imgs.extend([self._img_file_name(path,
                                                      os.path.join(root, f))
@@ -266,7 +288,7 @@ class Loader(loader.Loader):
                                  f.find("-256") < 0])
                 self._files[set_name] = (imgs, index)
                 index += len(imgs)
-            self.debug("Saving files table to DB...")
+            self.info("Saving files table to DB...")
             self._db_.Put(files_key, json.dumps(self._files).encode())
             self.info("Initialized files table")
         self._files_locator = sorted([(files[1], files[0], set_name)
@@ -286,16 +308,17 @@ class Loader(loader.Loader):
     def _set_meta(self, index, value):
         self._db_.Put(self._gen_img_key(index), json.dumps(value).encode())
 
-    def _init_metadata(self, force=False):
+    def _init_metadata(self):
         self.debug("Initializing metadata...")
         metadata_key = ("metadata_%s_%s" % (self.year, self.series)).encode()
-        if not force:
+        # self._db_.Delete(metadata_key)
+        if not self._force_reinit:
             try:
                 self._db_.Get(metadata_key)
                 self.info("Found metadata in DB")
                 return
             except KeyError:
-                pass
+                self.info("Initializing metadata from scratch...")
         self.debug("Will look for metadata in %s", self._ipath)
         all_xmls = {}
         for set_name, years in Loader.MAPPING.items():
@@ -304,12 +327,12 @@ class Loader(loader.Loader):
             if not subdir:
                 continue
             path = os.path.join(self._ipath, subdir)
-            self.debug("Scanning %s...", path)
+            self.info("Scanning %s...", path)
             for root, _, files in os.walk(path, followlinks=True):
                 xmls.extend([os.path.join(root, f)
                              for f in files
                              if os.path.splitext(f)[1] == ".xml"])
-        self.debug("Building image indices mapping")
+        self.info("Building image indices mapping")
         ifntbl = {}
         for set_name, files in self._files.items():
             flist = files[0]
@@ -321,7 +344,7 @@ class Loader(loader.Loader):
                 self.error("Duplicate file names detected in %s (%s, %s)",
                            set_name, self.year, self.series)
             ifntbl[set_name] = table
-        self.debug("Parsing XML files...")
+        self.info("Parsing XML files...")
         progress = ProgressBar(maxval=sum(
             [len(xmls) for xmls in all_xmls.values()]))
         progress.start()
@@ -349,6 +372,52 @@ class Loader(loader.Loader):
         self._db_.Put(metadata_key, b"")
         self.info("Initialized metadata")
 
+    def _init_labels(self):
+        self.debug("Initializing labels...")
+        label_key = ("labels_%s_%s" % (self.year, self.series)).encode()
+        # self._db_.Delete(label_key)
+        if not self._force_reinit:
+            try:
+                self._label_map = json.loads(self._db_.Get(label_key).decode())
+                self.info("Found %d labels in DB", len(self._label_map))
+                return
+            except KeyError:
+                self.info("Initializing labels from scratch...")
+        names = set()
+        self._metadata_misses = {}
+        progress = ProgressBar(maxval=sum(len(f[0]) for f
+                                              in self._files.values()))
+        progress.start()
+        for set_name, files in self._files.items():
+            flist = files[0]
+            base = files[1]
+            self._metadata_misses[set_name] = 0
+            for i in range(len(flist)):
+                progress.inc()
+                try:
+                    meta = self._get_meta(base + i)
+                    names.add(meta["object"]["name"])
+                except:
+                    self._metadata_misses[set_name] += 1
+                    names.add(os.path.basename(os.path.dirname(flist[i])))
+        progress.finish()
+        self.info("Sorting labels...")
+        label_indices = [n for n in names]
+        label_indices.sort()
+        self._label_map = {name: i for i, name in enumerate(label_indices)}
+        self.info("Saving labels to DB...")
+        self._db_.Put(label_key, json.dumps(self._label_map).encode())
+        self.info("Initialized %d labels", len(self._label_map))
+        table = PrettyTable("set", "files", "bbox", "bbox/files, %")
+        table.align["set"] = "l"
+        table.align["files"] = "l"
+        for set_name, files in self._files.items():
+            meta_count = len(files[0]) - self._metadata_misses[set_name]
+            table.add_row(set_name, len(files[0]), meta_count,
+                          int(meta_count * 100 / len(files[0])))
+        self.info("Stats:\n%s", str(table))
+
     def _fill_class_samples(self):
-        for key, val in loader.TRIAGE.items():
-            self.class_samples[val] = len(self._files[key][0])
+        for set_name, files in self._files.items():
+            index = loader.TRIAGE[set_name]
+            self.class_samples[index] = len(files[0])
