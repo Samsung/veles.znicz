76a054bc364c4db9683da72d3235cd185f48828e
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-05-06 16:53:48 +0400

    Attempted to fix Imagenet loader tests
    Change-Id: Ib11fa12dbbf1abdaf4b76dfbbca020469a1097db

--- samples/imagenet/test_loader.py
+++ samples/imagenet/test_loader.py
@@ -12,40 +12,39 @@ from veles.tests import DummyWorkflow
 from veles.znicz.samples.imagenet import LoaderDetection
 
 
-loader = None
-
-
 class Test(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        self.loader = LoaderDetection(DummyWorkflow(),
+                                      ipath="/data/imagenet/2013",
+                                      dbpath="/data/imagenet/2013/db",
+                                      year="2013", series="DET")
+        self.loader.setup(level=logging.DEBUG)
+        self.loader.load_data()
+
     def test_decode_image(self):
-        data = loader._decode_image(0)
+        data = self.loader._decode_image(0)
         self.assertEqual(3, len(data.shape))
         self.assertEqual(3, data.shape[2])
         self.assertGreater(data.shape[0], 100)
         self.assertGreater(data.shape[1], 100)
 
     def test_get_sample(self):
-        data = loader._get_sample(450000)
+        data = self.loader._get_sample(450000)
         self.assertEqual(3, len(data.shape))
         self.assertEqual(3, data.shape[2])
-        self.assertEqual(loader._data_shape[0], data.shape[0])
-        self.assertEqual(loader._data_shape[1], data.shape[1])
-        self.assertEqual(loader._dtype, data.dtype)
-        loader.include_derivative = True
-        loader._colorspace = "HSV"
-        data = loader._get_sample(450000)
+        self.assertEqual(self.loader._data_shape[0], data.shape[0])
+        self.assertEqual(self.loader._data_shape[1], data.shape[1])
+        self.assertEqual(self.loader._dtype, data.dtype)
+        self.loader.include_derivative = True
+        self.loader._colorspace = "HSV"
+        data = self.loader._get_sample(450000)
         self.assertEqual(3, len(data.shape))
         self.assertEqual(4, data.shape[2])
-        self.assertEqual(loader._data_shape[0], data.shape[0])
-        self.assertEqual(loader._data_shape[1], data.shape[1])
-        self.assertEqual(loader._dtype, data.dtype)
-        loader.include_derivative = False
-        loader._colorspace = "RGB"
+        self.assertEqual(self.loader._data_shape[0], data.shape[0])
+        self.assertEqual(self.loader._data_shape[1], data.shape[1])
+        self.assertEqual(self.loader._dtype, data.dtype)
+        self.loader.include_derivative = False
+        self.loader._colorspace = "RGB"
 
 if __name__ == "__main__":
-    loader = LoaderDetection(DummyWorkflow(),
-                             ipath="/data/imagenet/2013",
-                             dbpath="/data/imagenet/2013/db",
-                             year="2013", series="DET")
-    loader.setup(level=logging.DEBUG)
-    loader.load_data()
     unittest.main()
