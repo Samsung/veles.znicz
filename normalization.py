Fixed bug in normalization unit
Change-Id: I6a6b7617eb38d8db733862b9f9ccf85a1cc3f407

normalization.py
@@ -107,6 +107,9 @@ class LRNormalizerForward(LocalResponseNormalizer):
         self.input.unmap()
         self.execute_kernel(self._global_size_, self._local_size_).wait()
 
+    def run(self):
+        self.ocl_run()
+
 
 class LRNormalizerBackward(LocalResponseNormalizer):
     """
@@ -115,8 +118,8 @@ class LRNormalizerBackward(LocalResponseNormalizer):
     def __init__(self, workflow, **kwargs):
         self.output = None  # output of forward layer
         self.input = None  # input of forward layer
-        self.err_output = None  # output error of fwd layer, our input error
-        self.err_input = formats.Vector()  # input error of fwd layer, our output
+        self.err_output = None  # output error of fwd unit, our input error
+        self.err_input = formats.Vector()  # input error of fwd unit, our output
 
         super(LRNormalizerBackward, self).__init__(workflow, **kwargs)
 
@@ -172,19 +175,24 @@ class LRNormalizerBackward(LocalResponseNormalizer):
             min_index = max(0, i - int(self.n / 2))
             max_index = min(i + int(self.n / 2), num_of_chans - 1)
 
-            dh = np.zeros(dtype=np.float64,
+            delta_h = np.zeros(dtype=np.float64,
                           shape=delta_input[:, :, :, i].shape)
             for j in range(min_index, max_index + 1):
+                dh = np.zeros(shape=delta_h.shape, dtype=np.float64)
                 if i == j:
                     dh += input_subsums[:, :, :, j]
                 dh -= (2 * self.beta * self.alpha * self.input.v[:, :, :, i] *
                        self.input.v[:, :, :, j])
                 dh *= (delta_output[:, :, :, j] /
                        input_subsums_powered[:, :, :, j])
-            delta_input[:, :, :, i] += dh
+                delta_h += dh
+            delta_input[:, :, :, i] += delta_h
 
     def ocl_run(self):
         self.err_output.unmap()
         self.input.unmap()
         self.err_input.unmap()
         self.execute_kernel(self._global_size_, self._local_size_).wait()
+
+    def run(self):
+        self.ocl_run()

tests/unit/test_normalization.py
@@ -30,10 +30,11 @@ class TestNormalization(unittest.TestCase):
         fwd_normalizer = LRNormalizerForward(self.workflow,
                                              device=self.device, n=3)
         fwd_normalizer.input = Vector()
-        in_vector = np.zeros(shape=(1, 1, 5, 5), dtype=np.float64)
+        in_vector = np.zeros(shape=(1, 2, 5, 5), dtype=np.float64)
 
         for i in range(5):
             in_vector[0, 0, i, :] = np.linspace(10, 50, 5) * (i + 1)
+            in_vector[0, 1, i, :] = np.linspace(10, 50, 5) * (i + 1) + 1
 
         fwd_normalizer.input.v = in_vector
         fwd_normalizer.initialize(device=self.device)
@@ -51,35 +52,38 @@ class TestNormalization(unittest.TestCase):
         logging.info("FORWARD")
         self.assertLess(max_delta, 0.0001,
                         "Result differs by %.6f" % (max_delta))
+
         logging.info("FwdProp done.")
 
     def test_normalization_backward(self):
 
-        h = np.zeros(shape=(1, 1, 5, 5), dtype=np.float64)
+        h = np.zeros(shape=(1, 2, 5, 5), dtype=np.float64)
         for i in range(5):
             h[0, 0, i, :] = np.linspace(10, 50, 5) * (i + 1)
+            h[0, 1, i, :] = np.linspace(10, 50, 5) * (i + 1) + 1
 
-        err_y = np.zeros(shape=(1, 1, 5, 5), dtype=np.float64)
+        err_y = np.zeros(shape=(1, 2, 5, 5), dtype=np.float64)
         for i in range(5):
             err_y[0, 0, i, :] = np.linspace(2, 10, 5) * (i + 1)
+            err_y[0, 1, i, :] = np.linspace(2, 10, 5) * (i + 1) + 1
 
         back_normalizer = LRNormalizerBackward(self.workflow,
                                                device=self.device, n=3)
-        back_normalizer.h = Vector()
-        back_normalizer.err_y = Vector()
+        back_normalizer.input = Vector()
+        back_normalizer.err_output = Vector()
 
-        back_normalizer.h.v = h
-        back_normalizer.err_y.v = err_y
+        back_normalizer.input.v = h
+        back_normalizer.err_output.v = err_y
 
         back_normalizer.initialize(device=self.device)
 
         back_normalizer.cpu_run()
-        back_normalizer.err_h.map_read()
-        cpu_result = np.copy(back_normalizer.err_h.v)
+        back_normalizer.err_input.map_read()
+        cpu_result = np.copy(back_normalizer.err_input.v)
 
         back_normalizer.ocl_run()
-        back_normalizer.err_h.map_read()
-        ocl_result = np.copy(back_normalizer.err_h.v)
+        back_normalizer.err_input.map_read()
+        ocl_result = np.copy(back_normalizer.err_input.v)
 
         logging.info("BACK")
 
@@ -87,6 +91,11 @@ class TestNormalization(unittest.TestCase):
         assert max_delta < 0.0001
         self.assertLess(max_delta, 0.0001,
                         "Result differs by %.6f" % (max_delta))
+
+        print(err_y)
+        print(cpu_result)
+        print(ocl_result)
+
         logging.info("BackProp done.")
 
 