2395c8702b101a5b55e21390b55fd425dba89e09
Alexey Kazantsev <a.kazantsev@samsung.com>
2014-05-19 22:56:09 +0400

    Added numeric differentiation tests

--- tests/unit/test_gd.py
+++ tests/unit/test_gd.py
@@ -17,6 +17,7 @@ import veles.opencl_types as opencl_types
 import veles.random_generator as rnd
 import veles.znicz.gd as gd
 from veles.tests.dummy_workflow import DummyWorkflow
+import veles.znicz.all2all as all2all
 
 
 class TestGD(unittest.TestCase):
@@ -31,30 +32,43 @@ class TestGD(unittest.TestCase):
     def tearDown(self):
         del self.device
 
-    def _do_test(self, device, Unit):
+    def _do_test(self, device, Unit, Forward):
+        batch_size = 1
+        input_size = 25
+        n_neurons = 7
+
         rnd.get().state = self.state
 
-        inp = formats.Vector()
         dtype = opencl_types.dtypes[root.common.dtype]
-        inp.mem = numpy.empty([1, 1], dtype=dtype)
-        rnd.get().fill(inp.mem)
-
-        if device is not None:
-            self.x = inp.mem.copy()
-        else:
-            inp.mem[:] = self.x[:]
+        inp = numpy.empty([batch_size, input_size], dtype=dtype)
+        rnd.get().fill(inp)
 
         c = Unit(DummyWorkflow(), gradient_moment=0.0)
-        c.input = inp
 
-        weights = numpy.empty([1, 1], dtype=dtype)
+        weights = numpy.empty([n_neurons, input_size], dtype=dtype)
         rnd.get().fill(weights)
-        bias = numpy.empty(1, dtype=dtype)
-        rnd.get().fill(bias)
-        c.err_output = formats.Vector()
-        c.err_output.mem = numpy.empty([1, 1], dtype=dtype)
-        rnd.get().fill(c.err_output.mem)
 
+        bias = numpy.empty(n_neurons, dtype=dtype)
+        rnd.get().fill(bias)
+
+        err_output = numpy.empty([batch_size, n_neurons], dtype=dtype)
+        rnd.get().fill(err_output)
+
+        if device is not None:
+            self.x = inp.copy()
+            self.weights = weights.copy()
+            self.bias = bias.copy()
+            self.err_output = err_output.copy()
+        else:
+            inp[:] = self.x[:]
+            weights[:] = self.weights[:]
+            bias[:] = self.bias[:]
+            err_output[:] = self.err_output[:]
+
+        c.input = formats.Vector()
+        c.input.mem = inp.copy()
+        c.err_output = formats.Vector()
+        c.err_output.mem = err_output.copy()
         c.weights = formats.Vector()
         c.weights.mem = weights
         c.bias = formats.Vector()
@@ -83,11 +97,48 @@ class TestGD(unittest.TestCase):
             self.b_gpu = c.bias.mem.copy()
         c.err_input.map_read()  # get results back
 
+        logging.info("Will check with numeric differentiation")
+        forward = Forward(DummyWorkflow(), output_shape=err_output.shape[1:])
+        forward.input = formats.Vector()
+        forward.input.mem = inp.copy()
+        forward.initialize(device=self.device)
+        forward.weights.map_invalidate()
+        forward.weights.mem[:] = weights[:]
+        forward.bias.map_invalidate()
+        forward.bias.mem[:] = bias[:]
+        forward.run()
+        forward.output.map_read()
+        target = forward.output.mem.ravel() - err_output.ravel()
+        h = 1.0e-8
+        points = (2.0 * h, h, -h, -2.0 * h)
+        coeffs = numpy.array([-1.0, 8.0, -8.0, 1.0], dtype=numpy.float64)
+        divizor = 12.0 * h
+        errs = numpy.zeros_like(points)
+        err_input = c.err_input.mem.ravel()
+        offs = 0
+        for i_sample in range(inp.shape[0]):
+            for x in range(inp.shape[1]):
+                for i, p in enumerate(points):
+                    forward.input.map_invalidate()
+                    forward.input.mem[:] = inp[:]
+                    forward.input.mem[i_sample, x] = inp[i_sample, x] + p
+                    forward.run()
+                    forward.output.map_read()
+                    out = forward.output.mem.ravel()
+                    errs[i] = numpy.square(out - target).sum() * 0.5
+
+                derivative = (errs * coeffs).sum() / divizor
+                d = numpy.fabs(derivative - err_input[offs])
+                logging.info("%.2f %.2f %.2f" %
+                             (derivative, err_input[offs], d))
+                self.assertLess(d, 0.5, "Numeric diff test failed")
+                offs += 1
+
         return c.err_input.mem.copy()
 
-    def _do_test_gpu_cpu(self, Unit):
-        output_gpu = self._do_test(self.device, Unit)
-        output_cpu = self._do_test(None, Unit)
+    def _do_test_gpu_cpu(self, Unit, Forward):
+        output_gpu = self._do_test(self.device, Unit, Forward)
+        output_cpu = self._do_test(None, Unit, Forward)
         max_diff = numpy.fabs(output_gpu.ravel() - output_cpu.ravel()).max()
         self.assertLess(max_diff, 0.0001,
                         "Result differs by %.6f" % (max_diff))
@@ -98,19 +149,19 @@ class TestGD(unittest.TestCase):
 
     def test_gpu_cpu_linear(self):
         logging.info("Will test linear gd unit for gpu/cpu correctness")
-        self._do_test_gpu_cpu(gd.GradientDescent)
+        self._do_test_gpu_cpu(gd.GradientDescent, all2all.All2All)
 
-    def test_gpu_cpu_relu(self):
+    def _test_gpu_cpu_relu(self):
         logging.info("Will test RELU gd unit for gpu/cpu correctness")
-        self._do_test_gpu_cpu(gd.GDRELU)
+        self._do_test_gpu_cpu(gd.GDRELU, all2all.All2AllRELU)
 
-    def test_gpu_cpu_softmax(self):
+    def _test_gpu_cpu_softmax(self):
         logging.info("Will test SoftMax gd unit for gpu/cpu correctness")
-        self._do_test_gpu_cpu(gd.GDSM)
+        self._do_test_gpu_cpu(gd.GDSM, all2all.All2AllSoftmax)
 
-    def test_gpu_cpu_tanh(self):
+    def _test_gpu_cpu_tanh(self):
         logging.info("Will test Tanh gd unit for gpu/cpu correctness")
-        self._do_test_gpu_cpu(gd.GDTanh)
+        self._do_test_gpu_cpu(gd.GDTanh, all2all.All2AllTanh)
 
 
 if __name__ == "__main__":
--- tests/unit/test_gd_conv.py
+++ tests/unit/test_gd_conv.py
@@ -16,6 +16,7 @@ import veles.formats as formats
 import veles.opencl as opencl
 import veles.opencl_types as opencl_types
 import veles.znicz.gd_conv as gd_conv
+import veles.znicz.conv as conv
 from veles.tests.dummy_workflow import DummyWorkflow
 
 
@@ -28,18 +29,33 @@ class TestGDConv(unittest.TestCase):
     def tearDown(self):
         del self.device
 
-    def _test_err_h(self):
+    def test_err_h(self):
         logging.info("Will test convolutional layer back propagation")
 
-        inp = formats.Vector()
         dtype = opencl_types.dtypes[root.common.dtype]
-        inp.mem = numpy.array([[[-1, 0, 2, 0, 3],
-                              [0, 1, -2, 1, 2],
-                              [2, 0, 1, 1, 0],
-                              [-1, 1, 1, 0, 2],
-                              [1, 0, 1, 0, 1]]], dtype=dtype)
+        inp = numpy.array([[[-1, 0, 2, 0, 3],
+                            [0, 1, -2, 1, 2],
+                            [2, 0, 1, 1, 0],
+                            [-1, 1, 1, 0, 2],
+                            [1, 0, 1, 0, 1]],
+
+                           [[-1, 0, 2, 0, 3],
+                            [0, 1, -2, 1, 2],
+                            [2, 0, 1, 1, 0],
+                            [-1, 1, 1, 0, 2],
+                            [1, 0, 1, 0, 1]]], dtype=dtype)
 
         a = numpy.array([[-1, 0, 2, 0, 1, -2, 2, 0, 1],
+                         [0, 2, 0, 1, -2, 1, 0, 1, 1],
+                         [2, 0, 3, -2, 1, 2, 1, 1, 0],
+                         [0, 1, -2, 2, 0, 1, -1, 1, 1],
+                         [1, -2, 1, 0, 1, 1, 1, 1, 0],
+                         [-2, 1, 2, 1, 1, 0, 1, 0, 2],
+                         [2, 0, 1, -1, 1, 1, 1, 0, 1],
+                         [0, 1, 1, 1, 1, 0, 0, 1, 0],
+                         [1, 1, 0, 1, 0, 2, 1, 0, 1],
+
+                         [-1, 0, 2, 0, 1, -2, 2, 0, 1],
                          [0, 2, 0, 1, -2, 1, 0, 1, 1],
                          [2, 0, 3, -2, 1, 2, 1, 1, 0],
                          [0, 1, -2, 2, 0, 1, -1, 1, 1],
@@ -54,23 +70,36 @@ class TestGDConv(unittest.TestCase):
 
         bias = numpy.array([10, -10], dtype=dtype)
 
+        err_output = numpy.array([[[-1, 3],
+                                   [8, 2],
+                                   [0, 1],
+                                   [4, -1],
+                                   [-1, 2],
+                                   [0, 1],
+                                   [-2, 3],
+                                   [1, 2],
+                                   [1, 1]],
+
+                                  [[-1, 3],
+                                   [8, 2],
+                                   [0, 1],
+                                   [4, -1],
+                                   [-1, 2],
+                                   [0, 1],
+                                   [-2, 3],
+                                   [1, 2],
+                                   [1, 1]]], dtype=dtype)
+
         c = gd_conv.GradientDescentConv(DummyWorkflow(), n_kernels=2,
                                         kx=3, ky=3, gradient_moment=0.9)
         c.err_output = formats.Vector()
-        c.err_output.mem = numpy.array([[[-1, 3],
-                                       [8, 2],
-                                       [0, 1],
-                                       [4, -1],
-                                       [-1, 2],
-                                       [0, 1],
-                                       [-2, 3],
-                                       [1, 2],
-                                       [1, 1]]], dtype=dtype)
-        c.input = inp
+        c.err_output.mem = err_output.copy()
+        c.input = formats.Vector()
+        c.input.mem = inp.copy()
         c.weights = formats.Vector()
-        c.weights.mem = weights
+        c.weights.mem = weights.copy()
         c.bias = formats.Vector()
-        c.bias.mem = bias
+        c.bias.mem = bias.copy()
         c.output = formats.Vector()
         c.output.mem = c.err_output.mem.copy()
 
@@ -96,13 +125,20 @@ class TestGDConv(unittest.TestCase):
                           [2, 6, 93, -11, 0],
                           [22, 45, 18, 28, 7],
                           [-1, 11, 25, 14, 3],
+                          [14, 4, 13, 12, 5]],
+
+                         [[7, 0, -11, 31, -1],
+                          [2, 6, 93, -11, 0],
+                          [22, 45, 18, 28, 7],
+                          [-1, 11, 25, 14, 3],
                           [14, 4, 13, 12, 5]]], dtype=dtype)
         max_diff = numpy.fabs(t.ravel() - c.err_input.mem.ravel()).max()
         self.assertLess(max_diff, 0.0001,
                         "Result differs by %.6f" % (max_diff))
         logging.info("Err_input is right")
 
-        max_diff = numpy.fabs(weights_new.ravel() - c.weights.mem.ravel()).max()
+        max_diff = numpy.fabs(weights_new.ravel() -
+                              c.weights.mem.ravel()).max()
         self.assertLess(max_diff, 0.0001,
                         "Result differs by %.6f" % (max_diff))
         logging.info("Weights is right")
@@ -111,6 +147,45 @@ class TestGDConv(unittest.TestCase):
         self.assertLess(max_diff, 0.0001,
                         "Result differs by %.6f" % (max_diff))
         logging.info("Bias is right")
+
+        logging.info("Will check with numeric differentiation")
+        forward = conv.Conv(DummyWorkflow(), n_kernels=2, kx=3, ky=3)
+        forward.input = formats.Vector()
+        forward.input.mem = inp.copy()
+        forward.initialize(device=self.device)
+        forward.weights.map_invalidate()
+        forward.weights.mem[:] = weights[:]
+        forward.bias.map_invalidate()
+        forward.bias.mem[:] = bias[:]
+        forward.run()
+        forward.output.map_read()
+        target = forward.output.mem.ravel() - err_output.ravel()
+        h = 1.0e-8
+        points = (2.0 * h, h, -h, -2.0 * h)
+        coeffs = numpy.array([-1.0, 8.0, -8.0, 1.0], dtype=numpy.float64)
+        divizor = 12.0 * h
+        errs = numpy.zeros_like(points)
+        err_input = c.err_input.mem.ravel()
+        offs = 0
+        for i_sample in range(inp.shape[0]):
+            for y in range(inp.shape[1]):
+                for x in range(inp.shape[2]):
+                    for i, p in enumerate(points):
+                        forward.input.map_invalidate()
+                        forward.input.mem[:] = inp[:]
+                        forward.input.mem[i_sample, y, x] = (
+                            inp[i_sample, y, x] + p)
+                        forward.run()
+                        forward.output.map_read()
+                        out = forward.output.mem.ravel()
+                        errs[i] = numpy.square(out - target).sum() * 0.5
+
+                    derivative = (errs * coeffs).sum() / divizor
+                    d = numpy.fabs(derivative - err_input[offs])
+                    logging.info("%.2f %.2f %.2f" %
+                                 (derivative, err_input[offs], d))
+                    self.assertLess(d, 0.5, "Numeric diff test failed")
+                    offs += 1
 
     def test_err_h_cpu(self):
         logging.info("Will test CPU convolutional layer back propagation")
@@ -185,7 +260,8 @@ class TestGDConv(unittest.TestCase):
                         "Result differs by %.6f" % (max_diff))
         logging.info("Err_input is right")
 
-        max_diff = numpy.fabs(weights_new.ravel() - c.weights.mem.ravel()).max()
+        max_diff = numpy.fabs(weights_new.ravel() -
+                              c.weights.mem.ravel()).max()
         self.assertLess(max_diff, 0.0001,
                         "Result differs by %.6f" % (max_diff))
         logging.info("Weights is right")
@@ -195,16 +271,15 @@ class TestGDConv(unittest.TestCase):
                         "Result differs by %.6f" % (max_diff))
         logging.info("Bias is right")
 
-    def _test_padding_sliding(self):
+    def test_padding_sliding(self):
         logging.info("Will test convolutional layer back propagation")
 
-        inp = formats.Vector()
         dtype = opencl_types.dtypes[root.common.dtype]
-        inp.mem = numpy.array([[[1, 2, 3, 2, 1],
-                              [0, 1, 2, 1, 0],
-                              [0, 1, 0, 1, 0],
-                              [2, 0, 1, 0, 2],
-                              [1, 0, 1, 0, 1]]], dtype=dtype)
+        inp = numpy.array([[[1, 2, 3, 2, 1],
+                            [0, 1, 2, 1, 0],
+                            [0, 1, 0, 1, 0],
+                            [2, 0, 1, 0, 2],
+                            [1, 0, 1, 0, 1]]], dtype=dtype)
 
         a = numpy.array([[0, 0, 0, 0, 0, 0, 0, 1, 2],
                          [0, 0, 0, 0, 0, 0, 2, 3, 2],
@@ -225,29 +300,32 @@ class TestGDConv(unittest.TestCase):
 
         bias = numpy.array([10, -10], dtype=dtype)
 
+        err_output = numpy.array([[[-1, 3],
+                                   [8, 2],
+                                   [0, 1],
+                                   [4, -1],
+                                   [-1, 2],
+                                   [0, 1],
+                                   [-2, 3],
+                                   [1, 2],
+                                   [1, 1],
+                                   [1, -2],
+                                   [0, 5],
+                                   [2, 3]]], dtype=dtype)
+
         c = gd_conv.GradientDescentConv(DummyWorkflow(), n_kernels=2,
                                         kx=3, ky=3, padding=(1, 2, 3, 4),
                                         sliding=(2, 3), gradient_moment=0.9)
         c.err_output = formats.Vector()
-        c.err_output.mem = numpy.array([[[-1, 3],
-                                       [8, 2],
-                                       [0, 1],
-                                       [4, -1],
-                                       [-1, 2],
-                                       [0, 1],
-                                       [-2, 3],
-                                       [1, 2],
-                                       [1, 1],
-                                       [1, -2],
-                                       [0, 5],
-                                       [2, 3]]], dtype=dtype)
-        c.input = inp
+        c.err_output.mem = err_output.copy()
+        c.input = formats.Vector()
+        c.input.mem = inp.copy()
         c.weights = formats.Vector()
-        c.weights.mem = weights
+        c.weights.mem = weights.copy()
         c.bias = formats.Vector()
-        c.bias.mem = bias
+        c.bias.mem = bias.copy()
         c.output = formats.Vector()
-        c.output.mem = c.err_output.mem.copy()
+        c.output.mem = err_output.copy()
 
         batch_size = c.err_output.mem.shape[0]
         b = c.err_output.mem.reshape(12 * batch_size, 2)
@@ -275,7 +353,7 @@ class TestGDConv(unittest.TestCase):
         self.assertLess(max_diff, 0.0001,
                         "Result differs by %.6f\nTarget is:\n%s\nGot:\n%s" %
                         (max_diff, " ".join("%.2f" % x for x in t.ravel()),
-                         " ".join("%.2f" % x for x in c.err_input.mem.ravel())))
+                        " ".join("%.2f" % x for x in c.err_input.mem.ravel())))
         logging.info("Err_input is right")
 
         max_diff = numpy.fabs(bias_new.ravel() - c.bias.mem.ravel()).max()
@@ -283,13 +361,55 @@ class TestGDConv(unittest.TestCase):
                         "Result differs by %.6f" % (max_diff))
         logging.info("Bias is right")
 
-        max_diff = numpy.fabs(weights_new.ravel() - c.weights.mem.ravel()).max()
+        max_diff = numpy.fabs(weights_new.ravel() -
+                              c.weights.mem.ravel()).max()
         self.assertLess(max_diff, 0.0001,
                         "Result differs by %.6f" % (max_diff))
         logging.info("Weights is right")
 
+        logging.info("Will check with numeric differentiation")
+        forward = conv.Conv(DummyWorkflow(), n_kernels=2,
+                            kx=3, ky=3, padding=(1, 2, 3, 4),
+                            sliding=(2, 3))
+        forward.input = formats.Vector()
+        forward.input.mem = inp.copy()
+        forward.initialize(device=self.device)
+        forward.weights.map_invalidate()
+        forward.weights.mem[:] = weights[:]
+        forward.bias.map_invalidate()
+        forward.bias.mem[:] = bias[:]
+        forward.run()
+        forward.output.map_read()
+        target = forward.output.mem.ravel() - err_output.ravel()
+        h = 1.0e-8
+        points = (2.0 * h, h, -h, -2.0 * h)
+        coeffs = numpy.array([-1.0, 8.0, -8.0, 1.0], dtype=numpy.float64)
+        divizor = 12.0 * h
+        errs = numpy.zeros_like(points)
+        err_input = c.err_input.mem.ravel()
+        offs = 0
+        for i_sample in range(inp.shape[0]):
+            for y in range(inp.shape[1]):
+                for x in range(inp.shape[2]):
+                    for i, p in enumerate(points):
+                        forward.input.map_invalidate()
+                        forward.input.mem[:] = inp[:]
+                        forward.input.mem[i_sample, y, x] = (
+                            inp[i_sample, y, x] + p)
+                        forward.run()
+                        forward.output.map_read()
+                        out = forward.output.mem.ravel()
+                        errs[i] = numpy.square(out - target).sum() * 0.5
+
+                    derivative = (errs * coeffs).sum() / divizor
+                    d = numpy.fabs(derivative - err_input[offs])
+                    logging.info("%.2f %.2f %.2f" %
+                                 (derivative, err_input[offs], d))
+                    self.assertLess(d, 0.5, "Numeric diff test failed")
+                    offs += 1
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
