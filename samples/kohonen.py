5925a59efd97450df69ab65b2b069de399a21111
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-05-20 10:11:08 +0400

    Changed the amount of Kohonen's epochs to 160
    Change-Id: I2b4db8647394bd47741e1635b060c9257a73aa3d

--- samples/kohonen.py
+++ samples/kohonen.py
@@ -28,7 +28,7 @@ root.defaults = {
                 "weights_stddev": 0.05,
                 "weights_filling": "uniform"},
     "decision": {"snapshot_prefix": "kohonen",
-                 "epochs": 200},
+                 "epochs": 160},
     "loader": {"minibatch_maxsize": 10,
                "dataset_file": os.path.join(data_path, "kohonen.txt")},
     "train": {"gradient_decay": lambda t: 0.1 / (1.0 + t * 0.05),
