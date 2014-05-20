1be9ed4d03233d779f0f6dd04027e839ad70329a
Alexey Golovizin <a.golovizin@samsung.com>
2014-05-20 16:07:09 +0400

    Modified unit tests: added CAFFE pooling data
    Change-Id: I68c6a02439b609e117d6e2c1ea2ca4ecf9084eba

--- tests/unit/test_caffe.py
+++ tests/unit/test_caffe.py
@@ -12,6 +12,7 @@ import unittest
 from veles.formats import Vector
 
 import veles.znicz.conv as conv
+import veles.znicz.pooling as pooling
 from veles.tests.dummy_workflow import DummyWorkflow
 
 from scipy.signal import correlate2d
@@ -72,7 +73,7 @@ class TestConvCaffe(unittest.TestCase):
                         out_array[cur_pic, i, j, cur_chan] = data[j]
         return out_array
 
-    def test_caffe_version(self, data_path="data/convtest.txt"):
+    def test_caffe_conv(self, data_path="data/convtest.txt"):
         """
         Compare CAFFE conv layer fwd prop with Veles conv layer.
 
@@ -137,6 +138,74 @@ class TestConvCaffe(unittest.TestCase):
         delta_with_scipy = fwd_conv.output.mem - scipy_conv_out
         logging.info("Difference with SciPy: %.2f%%" % (100. * np.sum(np.abs(
             delta_with_scipy)) / np.sum(np.abs(fwd_conv.output.mem)),))
+
+    def test_caffe_pooling(self, data_path="/home/agolovizin/pooltest.txt"):
+        """
+        Compare CAFFE pooling unit fwd_prop with Veles one
+
+        Args:
+            data_path(str): path to file with pooling data, exported from CAFFE
+        """
+
+        #load pooling data from CAFFE dumps
+        in_file = open(data_path, 'r')
+        lines = in_file.readlines()
+        in_file.close()
+
+        #max pooling: 3x3 kernel, 2x2 stride
+        kernel_size = 3
+        stride = 2
+
+        in_height, in_width = 32, 32
+        out_height, out_width = 16, 16
+
+
+        bottom = self._read_array("bottom", lines=lines,
+                                  shape=(2, in_height, in_width, 2))
+        top = self._read_array("top", lines=lines,
+                               shape=(2, out_height, out_width, 2))
+
+
+        # do pooling with VELES
+        fwd_pool = pooling.MaxPooling(self.workflow, kx=kernel_size,
+                                      ky=kernel_size, sliding=(stride, stride))
+        fwd_pool.input = Vector()
+        fwd_pool.input.mem = bottom
+        fwd_pool.input.map_write()
+
+        fwd_pool.initialize(device=self.device)
+
+        fwd_pool.run()
+        fwd_pool.output.map_read()
+
+        # UNCOMMENT TO SEE EXTRA DEBUG DATA
+#        print(fwd_pool.output.mem)
+#        logging.info("top")
+#        print(top)
+
+#        print(np.sum(np.abs(fwd_pool.output.mem - top)) / np.sum(np.abs(top)))
+#        print(fwd_pool.output.mem - top)
+
+        #do MANUAL pooling
+        manual_pooling_out = np.zeros(shape=(2, out_height, out_width, 2),
+                                      dtype=np.float64)
+        for pic in range(2):
+            for chan in range(2):
+                for i_out in range(out_height):
+                    for j_out in range(out_width):
+                        min_i = i_out * 2
+                        max_i = i_out * 2 + 3
+                        min_j = j_out * 2
+                        max_j = j_out * 2 + 3
+
+                        zone = bottom[pic, min_i: max_i, min_j: max_j, chan]
+                        manual_pooling_out[pic, i_out, j_out, chan] = np.max(
+                                                                (zone))
+
+#        logging.info("CAFFE_top - manual")
+#        print(top - manual_pooling_out)
+
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
