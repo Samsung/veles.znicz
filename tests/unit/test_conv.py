43c4f5c29f691b67707b64571790137ef39372d0
Dmitry Senin <d.senin@samsung.com>
2014-05-26 15:12:24 +0400

    Added 2 unittests in test_conv.py for fixed input, weights and bias
    Change-Id: Id02adc8c92df5286d0c59127486a64839da2da66

--- tests/unit/test_conv.py
+++ tests/unit/test_conv.py
@@ -301,7 +301,6 @@ class TestConvWithPadding(TestConvBase):
                          ky=weights_shape[1], kx=weights_shape[2],
                          sliding=sliding, padding=padding)
 
-        logging.info("run conv with input = 1, weights = 1, bias = 1...")
         input_data = numpy.empty(input_shape)
         input_data.fill(1)
         weights = numpy.empty(weights_shape)
@@ -343,52 +342,51 @@ class TestConvWithPadding(TestConvBase):
         self._do_test_all_1(None, input_shape, kernels_num)
         logging.info("TEST PASSED")
 
-    def _do_test_padding_sliding(self):
-        # TODO: refactor this old impl.
-        logging.info("Will test convolutional layer forward propagation")
+    def _do_test_fixed_arrays(self, device):
+        """ Run test with fixed input data, weights and bias with tricky
+        padding and sliding.
 
-        inp = formats.Vector()
-        dtype = opencl_types.dtypes[root.common.dtype]
-        inp.mem = numpy.array([[[1, 2, 3, 2, 1],
-                              [0, 1, 2, 1, 0],
-                              [0, 1, 0, 1, 0],
-                              [2, 0, 1, 0, 2],
-                              [1, 0, 1, 0, 1]]], dtype=dtype)
+        Args:
+            device: OpenCL device instance (if value is equal to None, CPU
+                version of algorithm should be run. In other case -
+                OpenCL version).
+        """
+        input_data = numpy.array([[[[1], [2], [3], [2], [1]],
+                                   [[0], [1], [2], [1], [0]],
+                                   [[0], [1], [0], [1], [0]],
+                                   [[2], [0], [1], [0], [2]],
+                                   [[1], [0], [1], [0], [1]]]],
+                                 dtype=self._dtype)
 
         weights = numpy.array([[[-1, -1, -1],
                                 [-1, 8, -1],
                                 [-1, -1, -1]],
                                [[1.1, 2.1, 3.1],
                                 [-1.1, -0.5, 1.3],
-                                [1.7, -1.4, 0.05]]], dtype=dtype)
-        bias = numpy.array([10, -10], dtype=dtype)
+                                [1.7, -1.4, 0.05]]], dtype=self._dtype)
+        weights = weights.reshape(2, 3, 3, 1)
+        bias = numpy.array([10, -10], dtype=self._dtype)
+        gold_output = numpy.array(
+            [[[[7, -11.3], [3, -10.7], [7, -8], [10, -10]],
+              [[6, -8.4], [3, -2.8], [6, -12.8], [10, -10]],
+              [[9, -7.9], [9, -7.9], [9, -7.9], [10, -10]]]],
+            dtype=self._dtype)
 
-        c = conv.Conv(DummyWorkflow(), n_kernels=2, kx=3, ky=3,
+        unit = conv.Conv(DummyWorkflow(), n_kernels=2, kx=3, ky=3,
                       padding=(1, 2, 3, 4), sliding=(2, 3))
-        c.input = inp
+        self._run_test(unit, device, input_data, weights, bias, gold_output)
 
-        c.initialize(device=self.device)
+    def test_fixed_arrays_ocl(self):
+        logging.info("start testing OpenCL conv. layer forward propagation "
+                     "with fixed input data, weights and bias...")
+        self._do_test_fixed_arrays(opencl.Device())
+        logging.info("TEST PASSED")
 
-        c.weights.map_invalidate()  # rewrite weights
-        c.weights.mem[:] = weights.reshape(c.weights.mem.shape)[:]
-        c.bias.map_invalidate()  # rewrite bias
-        c.bias.mem[:] = bias[:]
-
-        c.run()
-        c.output.map_read()  # get results back
-        nz = numpy.count_nonzero(c.output.vv[c.output.mem.shape[0]:].ravel())
-        self.assertEqual(nz, 0, "Overflow occured")
-
-        y = c.output.mem.ravel()
-        t = numpy.array([[[7, -11.3], [3, -10.7], [7, -8], [10, -10]],
-                         [[6, -8.4], [3, -2.8], [6, -12.8], [10, -10]],
-                         [[9, -7.9], [9, -7.9], [9, -7.9], [10, -10]]],
-                        dtype=dtype).ravel()
-        max_diff = numpy.fabs(t - y).max()
-        self.assertLess(max_diff, 0.0001,
-                        "Result differs by %.6f" % (max_diff))
-
-        logging.info("All Ok")
+    def test_fixed_arrays_cpu(self):
+        logging.info("start testing CPU conv. layer forward propagation "
+                     "with fixed input data, weights and bias...")
+        self._do_test_fixed_arrays(None)
+        logging.info("TEST PASSED")
 
     def test_compare_ocl_vs_cpu(self):
         # TODO: implement
