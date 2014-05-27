50cda8dbe9c359ffecec3baf8545b06caa6bfbd6
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-05-27 11:25:28 +0400

    Fixed test_caffe PEP8
    Change-Id: Ieee568b32984b87c0fdb38cb0c61b5d46711f398

--- tests/unit/test_caffe.py
+++ tests/unit/test_caffe.py
@@ -492,8 +492,9 @@ class TestConvCaffe(unittest.TestCase):
 
         back_norm.err_input.map_read()
 
-        back_percent_delta = 100. * (np.sum(np.abs(back_norm.err_output.mem -
-            bot_err)) / np.sum(np.abs(bot_err)))
+        back_percent_delta = 100. * np.sum(
+            np.abs(back_norm.err_output.mem - bot_err)) / \
+            np.sum(np.abs(bot_err))
 
         logging.info("BACK NORM DELTA: %.2f%%" % back_percent_delta)
         self.assertLess(back_percent_delta, max_percent_delta,
