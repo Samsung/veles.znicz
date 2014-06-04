9e2dcfc8176702e1edfa3a2b161bddeb01761d63
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-06-04 20:14:06 +0400

    Added layers in imagenet_caffe config
    Change-Id: Ia452df624b8e80ba44bbce824c55579516228b7a

--- tests/research/imagenet_caffe.py
+++ tests/research/imagenet_caffe.py
@@ -15,12 +15,61 @@ from veles.znicz.standard_workflow import StandardWorkflow
 
 import logging
 
-root.defaults = {"decision": {"fail_iterations": 100,
-                              "store_samples_mse": True},
-                 "snapshotter": {"prefix": "imagenet_caffe"},
-                 "loader": {"minibatch_maxsize": 60},
-                 "imagenet_caffe": {"learning_rate": 0.01,
-                                    "weights_decay": 0.0}}
+root.defaults = {
+    "decision": {"fail_iterations": 100,
+                 "store_samples_mse": True},
+    "snapshotter": {"prefix": "imagenet_caffe"},
+    "loader": {"minibatch_maxsize": 60},
+    "imagenet_caffe": {"learning_rate": 0.01,
+                       "weights_decay": 0.0,
+                       "layers":
+                       [{"type": "conv_relu", "n_kernels": 96,
+                         "kx": 11, "ky": 11, "padding": (0, 0, 0, 0),
+                         "sliding": (4, 4),
+                         "weights_filling": "gaussian",
+                         "weights_stddev": 0.01},
+                        {"type": "max_pooling",
+                         "kx": 3, "ky": 3, "sliding": (2, 2)},
+                        {"type": "norm", "alpha": 0.00005,
+                         "beta": 0.75, "n": 3},
+
+                        {"type": "conv_relu", "n_kernels": 256,
+                         "kx": 5, "ky": 5, "padding": (2, 2, 2, 2),
+                         "sliding": (1, 1),
+                         "weights_filling": "gaussian",
+                         "weights_stddev": 0.01},
+                        {"type": "max_pooling",
+                         "kx": 3, "ky": 3, "sliding": (2, 2)},
+
+                        {"type": "conv", "n_kernels": 384,
+                         "kx": 3, "ky": 3, "padding": (1, 1, 1, 1),
+                         "sliding": (1, 1),
+                         "weights_filling": "gaussian",
+                         "weights_stddev": 0.01},
+
+                        {"type": "conv", "n_kernels": 384,
+                         "kx": 3, "ky": 3, "padding": (1, 1, 1, 1),
+                         "sliding": (1, 1),
+                         "weights_filling": "gaussian",
+                         "weights_stddev": 0.01},
+
+                        {"type": "conv_relu", "n_kernels": 256,
+                         "kx": 3, "ky": 3, "padding": (1, 1, 1, 1),
+                         "sliding": (1, 1),
+                         "weights_filling": "gaussian",
+                         "weights_stddev": 0.01},
+                        {"type": "max_pooling",
+                         "kx": 3, "ky": 3, "sliding": (2, 2)},
+
+                        {"type": "all2all_relu", "output_shape": 4096,
+                         "weights_filling": "gaussian",
+                         "weights_stddev": 0.005},
+
+                        {"type": "dropout", "dropout_ratio": 0.5},
+
+                        {"type": "softmax", "output_shape": 1000,
+                         "weights_filling": "gaussian",
+                         "weights_stddev": 0.01}]}}
 
 
 class Workflow(StandardWorkflow):
@@ -172,6 +221,6 @@ class Workflow(StandardWorkflow):
 
 
 def run(load, main):
-    load(Workflow)
+    load(Workflow, layers=root.imagenet_caffe.layers)
     main(learning_rate=root.imagenet_caffe.learning_rate,
          weights_decay=root.imagenet_caffe.weights_decay)
