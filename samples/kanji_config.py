5700d325db7ec1874d3f31ca00b1613ebfab2e03
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-09-26 20:13:31 +0400

    Fixed kanji
    Change-Id: I685c0407a10eb933a17f7e85456e354add9a223b

--- samples/kanji_config.py
+++ samples/kanji_config.py
@@ -21,13 +21,13 @@ train_path = os.path.join(root.common.test_dataset_root, "kanji/train")
 root.update = {
     "decision": {"fail_iterations": 1000,
                  "store_samples_mse": True},
-    "loader": {"minibatch_size": 5103,
+    "loader": {"minibatch_size": 50,
                "validation_ratio": 0.15},
     "snapshotter": {"prefix": "kanji"},
     "weights_plotter": {"limit": 16},
-    "kanji": {"learning_rate": 0.0000001,
+    "kanji": {"learning_rate": 0.00001,
               "weights_decay": 0.00005,
-              "layers": [5103, 2889, 24 * 24],
+              "layers": [250, 250, 24 * 24],
               "data_paths":
               {"target":
                os.path.join(root.common.test_dataset_root,
