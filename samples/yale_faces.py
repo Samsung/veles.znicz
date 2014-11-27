7d0e010a1a68b104f455890ce7f0cdbd156a1902
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-11-27 20:05:53 +0300

    Modified yale_faces model to new standard workflow
    Change-Id: If3f02c8e0ce0ff28d558c93ea59e15ab8b55ed1d

--- samples/yale_faces.py
+++ samples/yale_faces.py
@@ -16,40 +16,29 @@ from veles.config import root
 from veles.znicz.loader import ImageLoader
 from veles.znicz.standard_workflow import StandardWorkflow
 
-common_dir = root.common.test_dataset_root
-data_dir = os.path.join(common_dir, "CroppedYale")
-
-root.yalefaces.update({
-    "decision": {"fail_iterations": 50, "max_epochs": 1000},
-    "loss_function": "softmax",
-    "snapshotter": {"prefix": "yalefaces"},
-    "loader": {"minibatch_size": 40, "on_device": True,
-               "validation_ratio": 0.15,
-               "url":
-               "http://vision.ucsd.edu/extyaleb/CroppedYaleBZip/"
-               "CroppedYale.zip"},
-    "layers": [{"type": "all2all_tanh", "learning_rate": 0.01,
-                "weights_decay": 0.00005, "output_shape": 100},
-               {"type": "softmax", "output_shape": 39, "learning_rate": 0.01,
-                "weights_decay": 0.00005}]})
-
 
 class YaleFacesLoader(ImageLoader):
+    def __init__(self, workflow, **kwargs):
+        super(YaleFacesLoader, self).__init__(workflow, **kwargs)
+        self.data_dir = kwargs.get("data_dir", "")
+        self.common_dir = kwargs.get("common_dir", "")
+
     def load_data(self):
-        if not os.path.exists(data_dir):
+        if not os.path.exists(self.data_dir):
             url = root.yalefaces.loader.url
             self.warning("%s does not exist, downloading from %s...",
-                         data_dir, url)
-            dir_zip = wget.download("%s" % url, common_dir)
+                         self.data_dir, url)
+            dir_zip = wget.download("%s" % url, self.common_dir)
             try:
-                with zipfile.ZipFile(dir_zip) as zip:
-                    zip.extractall(common_dir)
+                with zipfile.ZipFile(dir_zip) as zip_file:
+                    zip_file.extractall(self.common_dir)
             finally:
                 os.remove(dir_zip)
         self.info("Loading from original Yale Faces files...")
         super(YaleFacesLoader, self).load_data()
-        self.extract_validation_from_train(
-            ratio=root.yalefaces.loader.validation_ratio)
+        if self.shuffled_indices.mem is None:
+            self.extract_validation_from_train(
+                ratio=root.yalefaces.loader.validation_ratio)
 
     def get_label_from_filename(self, filename):
         dn = os.path.dirname(filename)
@@ -61,25 +50,22 @@ class YaleFacesLoader(ImageLoader):
 
 
 class YaleFacesWorkflow(StandardWorkflow):
-    def __init__(self, workflow, **kwargs):
-        super(YaleFacesWorkflow, self).__init__(
-            workflow,
-            fail_iterations=root.yalefaces.decision.fail_iterations,
-            max_epochs=root.yalefaces.decision.max_epochs,
-            prefix=root.yalefaces.snapshotter.prefix,
-            snapshot_dir=root.common.snapshot_dir,
-            layers=root.yalefaces.layers,
-            loss_function=root.yalefaces.loss_function, **kwargs)
-
-    def link_loader(self):
-        super(YaleFacesWorkflow, self).link_loader()
+    def link_loader(self, init_unit):
         self.loader = YaleFacesLoader(
             self, on_device=root.yalefaces.loader.on_device,
-            train_paths=[data_dir],
-            minibatch_size=root.yalefaces.loader.minibatch_size)
-        self.loader.link_from(self.repeater)
+            minibatch_size=root.yalefaces.loader.minibatch_size,
+            train_paths=[root.yalefaces.loader.data_dir],
+            data_dir=root.yalefaces.loader.data_dir,
+            common_dir=root.yalefaces.loader.common_dir)
+        self.loader.link_from(init_unit)
 
 
 def run(load, main):
-    load(YaleFacesWorkflow)
+    load(YaleFacesWorkflow,
+         fail_iterations=root.yalefaces.decision.fail_iterations,
+         max_epochs=root.yalefaces.decision.max_epochs,
+         prefix=root.yalefaces.snapshotter.prefix,
+         snapshot_dir=root.common.snapshot_dir,
+         layers=root.yalefaces.layers,
+         loss_function=root.yalefaces.loss_function)
     main()
