3fef77708b385b419770618df330da93154d9e77
Alexey Golovizin <a.golovizin@samsung.com>
2014-12-01 20:31:25 +0300

    Added ChannelMerger class
    Change-Id: I61c4ac4c0b0da25d510d4eaf6955d0c538475e0f

--- channel_splitting.py
+++ channel_splitting.py
@@ -36,11 +36,12 @@ class ChannelSplitter(Forward):
                            dtype=self.input.mem.dtype)
 
         self.assign_kernel("split_channels")
-        self.set_args(self.input, self.output,
-                      numpy.array([self.input.shape[0]], dtype=numpy.int32),
-                      numpy.array([self.input.shape[3]], dtype=numpy.int32),
-                      numpy.array([self.input.shape[1]], dtype=numpy.int32),
-                      numpy.array([self.input.shape[2]], dtype=numpy.int32))
+        self.set_args(
+            self.input, self.output,
+            numpy.array([self.input.shape[0]], dtype=numpy.int32),  # n_pics
+            numpy.array([self.input.shape[3]], dtype=numpy.int32),  # n_chans
+            numpy.array([self.input.shape[1]], dtype=numpy.int32),  # pic_h
+            numpy.array([self.input.shape[2]], dtype=numpy.int32))  # pic_w
 
         self._global_size = [self.input.shape[1]]
 
@@ -52,6 +53,56 @@ class ChannelSplitter(Forward):
     def ocl_run(self):
         """Forward propagation from batch on GPU.
         """
+        self.output.unmap()
+        self.input.unmap()
+        self.execute_kernel(self._global_size, None)
+
+
+@implementer(IOpenCLUnit)
+class ChannelMerger(Forward):
+    def __init__(self, workflow, **kwargs):
+        super(ChannelMerger, self).__init__(workflow, **kwargs)
+
+    def init_unpickled(self):
+        super(ChannelSplitter, self).init_unpickled()
+        self.cl_sources_["channel_splitting.cl"] = {}
+
+    def initialize(self, device, **kwargs):
+        super(ChannelSplitter, self).initialize(device=device, **kwargs)
+        if (self.output.mem is None or
+                self.output.mem.size != self.input.mem.size):
+            self.output.reset()
+            self.output.mem = numpy.zeros(
+                dtype=self.input.mem.dtype,
+                shape=(self.input.mem.shape[0], self.input.mem.shape[2],
+                       self.input.mem.shape[3], self.input.mem.shape[1]))
+
+        self.input.initialize(self)
+        self.output.initialize(self)
+
+        if device is not None:
+            self.ocl_init(device)
+
+    def ocl_init(self, device):
+        self.build_program({}, "channel_splitting.cl",
+                           dtype=self.input.mem.dtype)
+
+        self.assign_kernel("merge_channels")
+        self.set_args(
+            self.input, self.output,
+            numpy.array([self.input.shape[0]], dtype=numpy.int32),  # n_pics
+            numpy.array([self.input.shape[1]], dtype=numpy.int32),  # n_chans
+            numpy.array([self.input.shape[2]], dtype=numpy.int32),  # pic_h
+            numpy.array([self.input.shape[3]], dtype=numpy.int32))  # pic_w
+
+        self._global_size = [self.input.shape[2]]
+
+    def cpu_run(self):
+        self.output.map_invalidate()
+        self.input.map_read()
+        self.output.mem[:] = self.input.mem.swapaxes(1, 2).swapaxes(2, 3)[:]
+
+    def ocl_run(self):
         self.output.unmap()
         self.input.unmap()
         self.execute_kernel(self._global_size, None)
--- ocl/channel_splitting.cl
+++ ocl/channel_splitting.cl
@@ -21,3 +21,20 @@ __kernel void split_channels(__global dtype* input, __global dtype* output,
     }
   }
 }
+
+__kernel void merge_channels(__global dtype* input, __global dtype* output,
+                             int n_pics, int n_chans, int pic_h, int pic_w) {
+
+  size_t i = get_global_id(0);
+
+    for(int pic = 0; pic < n_pics; ++pic){
+    int pic_index = pic * n_chans * pic_h * pic_w;
+    for(int j = 0; j < pic_w; ++j) {
+      for(int chan = 0; chan < n_chans; ++chan) {
+        int nhwc_index = pic_index + i * pic_w * n_chans + j * n_chans + chan;
+        int nchw_index = pic_index + chan * pic_h * pic_w + i * pic_w + j;
+        output[nhwc_index] = input[nchw_index];
+      }
+    }
+  }
+}
