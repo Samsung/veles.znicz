81a5a0438fb53e81489cefac88b8e9b8653c4050
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-12-01 17:13:26 +0300

    Modified mnist rbm workflow and config
    Change-Id: I2b038bf5c897b43032bd47f2486ef7447677f92e

--- samples/rbm.py
+++ samples/rbm.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3 -O
 """
-Created on Mar 20, 2013
+Created on Nov 20, 20134
 
-File for MNIST dataset.
+Model created for digits recognition. Database - MNIST.
+Model - RBM Neural Network.
 
 Copyright (c) 2013 Samsung Electronics Co., Ltd.
 """
 
 import numpy
-import os
 import scipy.io
 from zope.interface import implementer
 
@@ -21,24 +21,13 @@ import veles.znicz.nn_units as nn_units
 import veles.znicz.rbm as RBM_units
 
 
-root.mnist.update({
-    "all2all": {"weights_stddev": 0.05},
-    "decision": {"fail_iterations": 100,
-                 "max_epochs": 1000000000},
-    "snapshotter": {"prefix": "mnist"},
-    "loader": {"minibatch_size": 128, "on_device": True},
-    "learning_rate": 0.03,
-    "weights_decay": 0.0005,  # 1.6%
-    "factor_ortho": 0.0})
-
-
 @implementer(loader.IFullBatchLoader)
-class MnistLoader(loader.FullBatchLoader):
-    """Loads MNIST dataset.
-    """
+class MnistRBMLoader(loader.FullBatchLoader):
+    def __init__(self, workflow, **kwargs):
+        super(MnistRBMLoader, self).__init__(workflow, **kwargs)
+        self.data_path = kwargs.get("data_path", "")
+
     def shuffle(self):
-        """Randomly shuffles the TRAIN dataset.
-        """
         if self.shuffled_indices.mem is None:
             self.shuffled_indices.mem = numpy.arange(self.total_samples,
                                                      dtype=numpy.int32)
@@ -59,8 +48,6 @@ class MnistLoader(loader.FullBatchLoader):
                 self.minibatch_labels[i] = self.original_labels[int(ii)]
 
     def load_data(self):
-        """Here we load MNIST data.
-        """
         self.train_indx = numpy.zeros((3, 1), dtype=numpy.int32)
         self.original_labels.mem = numpy.zeros([6400], dtype=numpy.int32)
         self.original_data.mem = numpy.zeros([6400, 196],
@@ -68,77 +55,77 @@ class MnistLoader(loader.FullBatchLoader):
         self.class_lengths[0] = 0
         self.class_lengths[1] = 0
         self.class_lengths[2] = 4400
-        init_data_path = os.path.abspath(os.path.join(
-            os.path.dirname(__file__), '..', 'tests', 'unit', 'data',
-            'rbm', 'test_rbm.mat'))
+        init_data_path = self.data_path
         init_data = scipy.io.loadmat(init_data_path)
 
         self.original_data.mem[:] = init_data["patches"][:]
 
 
 class MnistRBMWorkflow(nn_units.NNWorkflow):
-    """Workflow for MNIST dataset (handwritten digits recognition).
-    """
     def __init__(self, workflow, layers, **kwargs):
         super(MnistRBMWorkflow, self).__init__(workflow, **kwargs)
         self.repeater.link_from(self.start_point)
-# LOADER
-        self.loader = MnistLoader(
-            self, name="Mnist fullbatch loader",
-            minibatch_size=128,
-            on_device=root.mnist.loader.on_device)
+
+        # LOADER
+        self.loader = MnistRBMLoader(
+            self, name="Mnist RBM fullbatch loader",
+            minibatch_size=root.mnist_rbm.loader.minibatch_size,
+            on_device=root.mnist_rbm.loader.on_device,
+            data_path=root.mnist_rbm.loader.data_path)
         self.loader.link_from(self.repeater)
 
-# FORWARD UNIT
-        aa = RBM_units.All2AllRBM(
-            self, output_shape=1000,
-            weights_stddev=root.mnist.all2all.weights_stddev)
-        self.fwds.append(aa)
-        self.fwds[0].link_from(self.loader)
-        self.fwds[0].link_attrs(self.loader,
-                                ("input", "minibatch_data"),
-                                ("batch_size", "minibatch_size"))
-        self.fwds[0].load_weights = True
-# EVALUATOR
+        # FORWARD UNIT
+        all2all_rbm = RBM_units.All2AllRBM(
+            self, output_shape=root.mnist_rbm.all2all.output_shape,
+            weights_stddev=root.mnist_rbm.all2all.weights_stddev)
+        self.forwards.append(all2all_rbm)
+        self.forwards[0].link_from(self.loader)
+        self.forwards[0].link_attrs(
+            self.loader, ("input", "minibatch_data"),
+            ("batch_size", "minibatch_size"))
+        self.forwards[0].load_weights = True
+
+        # EVALUATOR
         self.evaluator = RBM_units.EvaluatorRBM(self)
-        self.evaluator.link_from(self.fwds[0])
-        self.evaluator.link_attrs(self.fwds[0],
-                                  ("input", "output"),
-                                  ("output", "input"),
-                                  ("ground_truth", "input"),
-                                  "vbias", "bias", "weights")
-        self.evaluator.link_attrs(self.loader, ("labels", "minibatch_labels"),
-                                  ("batch_size", "minibatch_size"),
-                                  ("max_samples_per_epoch", "total_samples"),
-                                  "minibatch_class", "class_lengths",
-                                  "last_minibatch", "max_minibatch_size")
-# DECISION
+        self.evaluator.link_from(self.forwards[-1])
+        self.evaluator.link_attrs(
+            self.forwards[-1], ("input", "output"),
+            ("output", "input"), ("ground_truth", "input"),
+            "vbias", "bias", "weights")
+        self.evaluator.link_attrs(
+            self.loader, ("labels", "minibatch_labels"),
+            ("batch_size", "minibatch_size"),
+            ("max_samples_per_epoch", "total_samples"),
+            "minibatch_class", "class_lengths",
+            "last_minibatch", "max_minibatch_size")
+
+        # DECISION
         self.decision = TrivialDecision(
-            self, fail_iterations=root.mnist.decision.fail_iterations,
-            max_epochs=2)
+            self, fail_iterations=root.mnist_rbm.decision.fail_iterations,
+            max_epochs=root.mnist_rbm.decision.max_epochs)
         self.decision.link_from(self.evaluator)
-        self.decision.link_attrs(self.loader,
-                                 "minibatch_class", "minibatch_size",
-                                 "last_minibatch", "class_lengths",
-                                 "epoch_ended", "epoch_number")
-# link attribute max_epochs from self.decision to self.evaluator
+        self.decision.link_attrs(
+            self.loader, "minibatch_class", "minibatch_size",
+            "last_minibatch", "class_lengths", "epoch_ended", "epoch_number")
+
+        # link attribute max_epochs from self.decision to self.evaluator
         self.evaluator.link_attrs(self.decision, "max_epochs")
-# INTERPRETER PYTHON
+
+        # INTERPRETER PYTHON
         self.ipython = Shell(self)
         self.ipython.link_from(self.decision)
         self.ipython.gate_skip = ~self.decision.epoch_ended
 
-# GRADIENT
+        # GRADIENT
         del self.gds[:]
         gd_unit = RBM_units.GradientDescentRBM(
-            self, learning_rate=root.mnist.learning_rate)
+            self, learning_rate=root.mnist_rbm.learning_rate)
         self.gds.append(gd_unit)
         self.gds[0].link_from(self.ipython)
-        self.gds[0].link_attrs(self.fwds[0], ("err_output", "output"))
-        self.gds[0].link_attrs(self.fwds[-1],
-                               ("output", "output"),
-                               ("input", "input"),
-                               "weights", "bias", "vbias")
+        self.gds[0].link_attrs(self.forwards[0], ("err_output", "output"))
+        self.gds[0].link_attrs(
+            self.forwards[-1], ("output", "output"),
+            ("input", "input"), "weights", "bias", "vbias")
         self.gds[-1].gate_skip = self.decision.gate_skip
         self.gds[-1].link_attrs(self.loader, ("batch_size", "minibatch_size"))
         self.gds[-1].need_err_input = False
@@ -149,14 +136,13 @@ class MnistRBMWorkflow(nn_units.NNWorkflow):
         self.loader.gate_block = self.decision.complete
 
     def initialize(self, learning_rate, weights_decay, device, **kwargs):
-        a = super(MnistRBMWorkflow, self).initialize(
+        numpy.random.seed(1337)
+        return super(MnistRBMWorkflow, self).initialize(
             learning_rate=learning_rate, weights_decay=weights_decay,
             device=device)
-        numpy.random.seed(1337)
-        return a
 
 
 def run(load, main):
     load(MnistRBMWorkflow)
-    main(learning_rate=root.mnist.learning_rate,
-         weights_decay=root.mnist.weights_decay)
+    main(learning_rate=root.mnist_rbm.learning_rate,
+         weights_decay=root.mnist_rbm.weights_decay)
--- samples/rbm_config.py
+++ samples/rbm_config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3 -O
 """
-Created on Mart 21, 2014
+Created on Nov 20, 20134
 
-Example of Mnist config.
+Model created for digits recognition. Database - MNIST.
+Model - RBM Neural Network.
 
 Copyright (c) 2013 Samsung Electronics Co., Ltd.
 """
@@ -13,25 +14,15 @@ import os
 from veles.config import root
 
 
-mnist_dir = mnist_dir = "veles/znicz/samples/MNIST"
-
-# optional parameters
-test_image_dir = os.path.join(mnist_dir, "t10k-images.idx3-ubyte")
-test_label_dir = os.path.join(mnist_dir, "t10k-labels.idx1-ubyte")
-train_image_dir = os.path.join(mnist_dir, "train-images.idx3-ubyte")
-train_label_dir = os.path.join(mnist_dir, "train-labels.idx1-ubyte")
-
-
-root.mnist.update({
-    "all2all": {"weights_stddev": 0.05},
-    "decision": {"fail_iterations": 300,
-                 "snapshot_prefix": "mnist"},
-    "loader": {"minibatch_size": 88, "on_device": True},
-    "learning_rate": 0.028557478339518444,
-    "weights_decay": 0.00012315096341168246,
-    "factor_ortho": 0.001,
-    "layers": [364, 10],
-    "data_paths": {"test_images": test_image_dir,
-                   "test_label": test_label_dir,
-                   "train_images": train_image_dir,
-                   "train_label": train_label_dir}})
+root.mnist_rbm.update({
+    "all2all": {"weights_stddev": 0.05, "output_shape": 1000},
+    "decision": {"fail_iterations": 100,
+                 "max_epochs": 100},
+    "snapshotter": {"prefix": "mnist_rbm"},
+    "loader": {"minibatch_size": 128, "on_device": True,
+               "data_path":
+               os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                            "tests/unit/data/rbm/test_rbm.mat")},
+    "learning_rate": 0.03,
+    "weights_decay": 0.0005,
+    "factor_ortho": 0.0})
