ac227f2fa3379faf256ab743698816ff663aa1e0
Alexey Golovizin <a.golovizin@samsung.com>
2014-12-02 19:06:58 +0300

    Added a test for CuDNN fwd conv
    Change-Id: Ie9531a1ea6c1b8af3898a874e7617b4f7deccfb2

--- tests/research/CUDNN/test_cudnn.py
+++ tests/research/CUDNN/test_cudnn.py
@@ -16,6 +16,8 @@ import veles.opencl as opencl
 from veles.dummy import DummyWorkflow
 
 from veles import units
+from veles.znicz import channel_splitting
+from veles.znicz import conv
 
 
 @implementer(units.IUnit)
@@ -60,6 +62,8 @@ class CUDNNConv(units.Unit):
 
         self._cur_context = None
 
+        self.output = formats.Vector()
+
     def initialize(self, device, **kwargs):
         self._n_pics, self._n_chans, self._pic_h, self._pic_w = \
             self.input.mem.shape
@@ -98,9 +102,9 @@ class CUDNNConv(units.Unit):
         cur_device = cuda.Device(0)
         self._cur_context = cur_device.make_context()
 
-        self.output = formats.Vector(numpy.zeros(
+        self.output.mem = numpy.zeros(
             shape=(self._n_pics, self._n_filters, out_h, out_w),
-            dtype=self.input.mem.dtype))
+            dtype=self.input.mem.dtype)
 
     def __del__(self):
         self._cur_context.pop()
@@ -129,35 +133,85 @@ class TestCUDNNBase(unittest.TestCase):
         root.common.unit_test = True
         root.common.plotters_disabled = True
         self.device = opencl.Device()
-        self.workflow = DummyWorkflow()
 
     def tearDown(self):
         pass
 
-    def test_all(self):
+    def get_cudnn_conv_result(self):
+        workflow = DummyWorkflow()
+
         filters = numpy.zeros(shape=(1, 3, 3, 3), dtype=numpy.float64)
         for chan in range(filters.shape[3]):
             filters[0, :, :, chan] = numpy.array([[1, 1, 1], [1, 1, 1],
                                                  [1, 1, 1]])
 
-        in_pic = cv2.imread("../data/FM.jpg")
+        in_pic = cv2.imread("../../data/FM.jpg")
 
-        input_data = numpy.array(cv2.split(in_pic), dtype=numpy.float64)
+        workflow.end_point.unlink_all()
+
+        input_data = numpy.array(in_pic, dtype=numpy.float64)
         input_data = input_data.reshape((1,) + input_data.shape)
 
-        cudnn_conv = CUDNNConv(self.workflow, padding=(0, 0), stride=(1, 1))
-        cudnn_conv.link_from(self.workflow.start_point)
-        self.workflow.end_point.unlink_all()
-        self.workflow.end_point.link_from(cudnn_conv)
+        chan_splitter = channel_splitting.ChannelSplitter(workflow)
+        chan_splitter.link_from(workflow.start_point)
+        chan_splitter.input = formats.Vector(input_data)
 
-        cudnn_conv.input = formats.Vector(input_data)
+        cudnn_conv = CUDNNConv(workflow, padding=(0, 0), stride=(1, 1))
+        cudnn_conv.link_from(chan_splitter)
+        cudnn_conv.link_attrs(chan_splitter, ("input", "output"))
         cudnn_conv.weights = formats.Vector(filters)
 
-        self.workflow.initialize(device=self.device)
-        self.workflow.run()
+        chan_merger = channel_splitting.ChannelMerger(workflow)
+        chan_merger.link_from(cudnn_conv)
+        chan_merger.link_attrs(cudnn_conv, ("input", "output"))
 
-        cv2.imshow("FM2", cudnn_conv.output.mem[0][0] / 255. / 3 / 10)
-        cv2.waitKey(0)
+        workflow.end_point.link_from(chan_merger)
+
+        workflow.initialize(device=self.device)
+        workflow.run()
+
+        chan_merger.output.map_read()
+        return chan_merger.output.mem.copy()
+
+    def get_standard_conv_result(self):
+        workflow = DummyWorkflow()
+        filters = numpy.zeros(shape=(1, 3, 3, 3), dtype=numpy.float64)
+        for chan in range(filters.shape[3]):
+            filters[0, :, :, chan] = numpy.array([[1, 1, 1], [1, 1, 1],
+                                                 [1, 1, 1]])
+        in_pic = cv2.imread("../data/FM.jpg")
+
+        workflow.end_point.unlink_all()
+
+        input_data = numpy.array(in_pic, dtype=numpy.float64)
+        input_data = input_data.reshape((1,) + input_data.shape)
+
+        standard_conv = conv.Conv(workflow, n_kernels=1, kx=3, ky=3,
+                                  sliding=(1, 1), padding=(0, 0, 0, 0))
+        standard_conv.link_from(workflow.start_point)
+        standard_conv.input = formats.Vector(input_data.copy())
+        standard_conv.weights = formats.Vector(filters.reshape((1, 27)).copy())
+
+        workflow.end_point.link_from(standard_conv)
+
+        workflow.initialize(device=self.device)
+
+        standard_conv.input.mem[:] = input_data[:]
+        standard_conv.weights.mem[:] = filters.reshape((1, 27))[:]
+
+        workflow.run()
+
+        standard_conv.output.map_read()
+        return standard_conv.output.mem.copy()
+
+    def test_conv(self):
+        standard_result = self.get_standard_conv_result()
+        cudnn_result = self.get_cudnn_conv_result()
+        delta = numpy.average((numpy.abs((standard_result - cudnn_result) /
+                                         numpy.maximum(standard_result,
+                                                       cudnn_result))))
+        logging.info("CuDNN to Veles conv: %.3f%%" % (float(delta) * 100))
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
