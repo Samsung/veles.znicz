42ac8b436c04d32c2fc1ef16ebe18a3615d1271d
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-12-05 13:53:44 +0300

    Bumped version to 0.5.0
    Change-Id: I5b6615fac2c527fa9f9bd6d72fa02aa4e2f0fe96

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ except ImportError:
     pass
 
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 __license__ = "Samsung Proprietary License"
 __copyright__ = "© 2013 Samsung Electronics Co., Ltd."
 
