d8c7347c461a0f9f1316769b4d67315b01165b6b
Lyubov Podoynitsina <lyubov.p@samsung.com>
2014-12-08 16:11:43 +0300

    Fixed kanji_standard test
    Change-Id: I4b2723346f41bdb0096edbc6c92e19e8bc1a82cd

--- tests/functional/test_kanji_standard.py
+++ tests/functional/test_kanji_standard.py
@@ -18,7 +18,7 @@ import veles.opencl as opencl
 import veles.prng as prng
 from veles.snapshotter import Snapshotter
 from veles.tests import timeout
-import veles.znicz.tests.samples.Kanji.kanji as kanji_standard
+from veles.znicz.tests.samples.Kanji import kanji as kanji_standard
 import veles.dummy as dummy_workflow
 
 
