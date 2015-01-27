Implemented Activation units on CUDA, fixed test_caffe
Change-Id: I0f23bf7baf839049ac9b863b9a38756724fa00ee

tests/unit/test_caffe.py
@@ -6,15 +6,12 @@ Unit test for convolutional layer forward propagation, compared to CAFFE data.
 Copyright (c) 2013 Samsung Electronics Co., Ltd.
 """
 
-import gc
 import logging
 import numpy as np
 import os
 from scipy.signal import correlate2d, convolve2d  # pylint: disable=E0611
 import unittest
 
-from veles import backends
-from veles.dummy import DummyWorkflow
 from veles.memory import Vector
 import veles.znicz.all2all as all2all
 import veles.znicz.conv as conv
@@ -28,16 +25,6 @@ from veles.znicz.tests.unit import standard_test
 
 
 class TestConvCaffe(standard_test.StandardTest):
-    def setUp(self):
-        self.workflow = DummyWorkflow()
-        self.device = backends.Device()
-        self.data_dir_path = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "data")
-
-    def tearDown(self):
-        gc.collect()
-        del self.device
-
     def test_caffe_conv(self, data_filename="conv.txt"):
         """
         Compare CAFFE conv layer fwd prop with Veles conv layer.
@@ -199,7 +186,7 @@ class TestConvCaffe(standard_test.StandardTest):
 
         back_conv.initialize(device=self.device)
 
-        back_conv.gpu_err_input_update()
+        back_conv.run()
 
         back_conv.err_input.map_read()
 
@@ -410,7 +397,7 @@ class TestConvCaffe(standard_test.StandardTest):
         fwd_norm.input = Vector(bottom)
 
         fwd_norm.initialize(self.device)
-        fwd_norm.ocl_run()
+        fwd_norm.run()
 
         fwd_norm.output.map_read()
 
@@ -431,7 +418,7 @@ class TestConvCaffe(standard_test.StandardTest):
         back_norm.err_output = Vector(top_err)
 
         back_norm.initialize(self.device)
-        back_norm.ocl_run()
+        back_norm.run()
 
         back_norm.err_input.map_read()
 
@@ -497,7 +484,7 @@ class TestConvCaffe(standard_test.StandardTest):
         fwd_conv_relu.bias.map_invalidate()
         fwd_conv_relu.bias.mem[:] = 0
 
-        fwd_conv_relu.ocl_run()
+        fwd_conv_relu.run()
 
         fwd_conv_relu.output.map_read()
 
@@ -567,7 +554,7 @@ class TestConvCaffe(standard_test.StandardTest):
         fwd_conv_relu.bias.map_invalidate()
         fwd_conv_relu.bias.mem[:] = 0
 
-        fwd_conv_relu.ocl_run()
+        fwd_conv_relu.run()
 
         fwd_conv_relu.output.map_read()
 

tests/unit/test_caffe_complex.py
@@ -7,7 +7,7 @@ Copyright (c) 2013 Samsung Electronics Co., Ltd.
 """
 
 import logging
-import numpy as np
+import numpy
 import os
 import unittest
 import tarfile
@@ -139,10 +139,11 @@ class ComplexTest(standard_test.StandardTest, Logger):
             float
         """
         assert array1.shape == array2.shape
-        return np.sum(np.abs(array1 - array2)) / np.sum(np.abs(array1)) * 100
+        return (numpy.sum(numpy.abs(array1 - array2)) /
+                numpy.sum(numpy.abs(array1)) * 100)
 #        a = array1.copy()
 #        b = array2.copy()
-#        return np.fabs(a - b).max() / np.fabs(a).max()
+#        return numpy.fabs(a - b).max() / numpy.fabs(a).max()
 
     def _create_fwd_units(self, iteration):
         # Layer 1: CONV+POOL
@@ -229,7 +230,7 @@ class ComplexTest(standard_test.StandardTest, Logger):
     def _load_labels_and_data(self, _iter):
         labels = self._load_blob(
             "loss", PropType.backward, WhenTaken.before,
-            "bottom_1", _iter).astype(np.int32).ravel()
+            "bottom_1", _iter).astype(numpy.int32).ravel()
 
         ev = self.workflow["ev"]
         if ev.labels is not None:
@@ -471,11 +472,20 @@ class ComplexTest(standard_test.StandardTest, Logger):
         self.workflow.end_point.link_from(self.workflow["gd_conv1"])
 
         self.workflow.initialize(device=self.device)
+        self.info("self.workflow.initialize() completed")
+
         self._load_labels_and_data(cur_iter)
+        self.info("_load_labels_and_data() completed")
 
         self._fill_weights(cur_iter)
+        self.info("_fill_weights() completed")
+
         self._load_labels_and_data(cur_iter)
+        self.info("_load_labels_and_data() completed")
+
+        self.info("Will execute run() now")
         self.workflow.run()
+        self.info("run() completed")
         self._print_deltas(cur_iter)
 
         cur_iter = 1