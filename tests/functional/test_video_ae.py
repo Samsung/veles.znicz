38b65651547e2f9d8e19635be0c6601a457208cb
Lyubov Podoynitsina <lyubov.p@samsung.com>
2015-03-04 14:24:07 +0300

    Fixed video_ae functional test after 2654 change
    Change-Id: I339c4b6d2ba0cb98b11c2fc4a99c5998e867b142

--- tests/functional/test_video_ae.py
+++ tests/functional/test_video_ae.py
@@ -70,7 +70,7 @@ class TestVideoAE(unittest.TestCase):
         file_name = self.w.snapshotter.file_name
 
         avg_mse = self.w.decision.epoch_metrics[2][0]
-        self.assertLess(avg_mse, 0.193783)
+        self.assertLess(avg_mse, 0.1957178)
         self.assertEqual(4, self.w.loader.epoch_number)
 
         logging.info("Will load workflow from %s" % file_name)
@@ -84,7 +84,7 @@ class TestVideoAE(unittest.TestCase):
         self.wf.run()
 
         avg_mse = self.wf.decision.epoch_metrics[2][0]
-        self.assertLess(avg_mse, 0.173972)
+        self.assertLess(avg_mse, 0.18736321)
         self.assertEqual(7, self.wf.loader.epoch_number)
         logging.info("All Ok")
 
